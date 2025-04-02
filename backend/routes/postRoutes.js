413bc3fbdf6702f25b3d787a0404de7e0c6ffc7b
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-02 11:18:47 -0500

    Create post routes

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,19 @@
+const postController = require("../controllers/postController");
+const router = require("express").Router();
+
+// Retrieve all posts
+router.get("/", postController.getAllPosts);
+
+// Create a new post
+router.post("/", postController.createPost);
+
+// Retrieve a specific post by ID
+router.get("/:postid", postController.getPost);
+
+// Update a specific post by ID
+router.put("/:postid", postController.editPost);
+
+// Delete a specific post by ID
+router.delete("/:postid", postController.deletePost);
+
+module.exports = { router };
