56046e7ee42db50c1bb3980d4f1e9250db1d5063
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-02 11:37:46 -0500

    Create comment routes in commentRoutes.js

---
+++ backend/routes/commentRoutes.js
@@ -0,0 +1,18 @@
+const commentController = require("../controllers/commentController");
+const router = express.Router({ mergeParams: true });
+
+// Routes for /posts/:postid/comments
+
+// Retrieve all comments from a post
+router.get("/", commentController.getAllPostComments);
+
+// Create a comment on a post
+router.post("/", commentController.createComment);
+
+// Edit a comment on a post
+router.put("/:commentid", commentController.editComment);
+
+// Delete a comment on a post
+router.delete("/:commentid", commentController.deleteComment);
+
+module.exports = router;
