71d71d845a0d26c71c98d8572dd15bce92541fe6
gdemko6 <gregory.demko@my.uwrf.edu>
2025-04-02 19:01:43 -0500

    Create queries for comment routes that perform the desired actions

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,45 @@
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const asyncHandler = require("express-async-handler");
+const prisma = new PrismaClient();
+
+const getAllPostComments = asyncHandler(async (req, res) => {
+  const { postid } = req.params;
+  const comments = await prisma.comment.findMany({
+    where: { postId: parseInt(postid, 10) },
+  });
+  res.status(200).json({ comments });
+});
+
+const createComment = asyncHandler(async (req, res) => {
+  const { postid } = req.params;
+  const { text, name } = req.body;
+  const createdComment = await prisma.comment.create({
+    data: {
+      text,
+      name,
+      post: {
+        connect: { id: parseInt(postid, 10) },
+      },
+    },
+  });
+  res.status(201).json({ createdComment });
+});
+
+const editComment = asyncHandler(async (req, res) => {
+  const { commentid } = req.params;
+  const { text, name } = req.body;
+  const updatedComment = await prisma.comment.update({
+    where: { id: parseInt(commentid, 10) },
+    data: { text, name },
+  });
+  res.status(200).json({ updatedComment });
+});
+
+const deleteComment = asyncHandler(async (req, res) => {
+  const { commentid } = req.params;
+  const deletedComment = await prisma.comment.delete({
+    where: { id: parseInt(commentid, 10) },
+  });
+  res.status(204).json({ deletedComment });
+});
